17669e85d1f70f26ba29b0d02f80252e19bbf20d
Zephilium <wuuuuush@gmail.com>
2021-06-04 19:11:50 +0700

    add images, font

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,64 +1,114 @@
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
+  var infomationTextStyle = TextStyle(fontFamily: 'Oxygen');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(top: 16.0),
-              child: Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset('images/farm-house.jpg'),
+              Container(
+                margin: EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Staathliches',
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8.0),
-                      Text('Open Everyday'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.timer),
-                      SizedBox(height: 8.0),
-                      Text('09.00 - 21.00'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.attach_money_rounded),
-                      SizedBox(height: 8.0),
-                      Text('Rp 25.000'),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16.0,
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.calendar_today),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Open Everyday',
+                          style: infomationTextStyle,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.timer),
+                        SizedBox(height: 8.0),
+                        Text(
+                          '09.00 - 21.00',
+                          style: infomationTextStyle,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.attach_money_rounded),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Rp 25.000',
+                          style: infomationTextStyle,
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              // Image.network(
+              //   'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
+              // ),
+              Container(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
